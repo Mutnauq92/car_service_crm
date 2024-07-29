929530fa90c2db95d16069db8273f002ab6891eb
Mutnauq92 <mohlabi16@gmail.com>
2024-07-29 21:57:07 +0200

    updated files

--- crm/models.py
+++ crm/models.py
@@ -3,9 +3,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.db import models
-from django.contrib.auth.models import User
-
 class CustomerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,6 @@
 # crm/views.py
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Car, Quotation, Appointment, CustomerProfile
